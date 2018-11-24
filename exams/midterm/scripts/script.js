b864b66e4d314794c95437b1808ca687c84d1e41
Euni <vescalaw@gmail.com>
2018-11-24 23:27:56 +0800

    Midterm Exam - Post Fixes

--- exams/midterm/scripts/script.js
+++ exams/midterm/scripts/script.js
@@ -1,12 +1,17 @@
 $(() => {
   $('.collapse')
-    .on('show.bs.collapse', () => $('.collapse[class*=\'show\']').collapse('hide'))
-    .on('shown.bs.collapse', ({ currentTarget: $this }) => {
-      const currentPage = $(`.collapse[class*=\'show\'] .display-2`).text();
+    .on('show.bs.collapse', () => $('.collapse.show').collapse('hide'))
+    .on('shown.bs.collapse', () => {
+      const currentPage = $(`.collapse.show .display-2`).text();
+
       document.title = currentPage;
       $(`.nav-link:contains('${currentPage}'):first`).addClass('active');
     })
-    .on('hide.bs.collapse', () => $(`.nav-link[class*='active']`).removeClass('active'));
+    .on('hide.bs.collapse', () => {
+      $(`.nav-link.active`).removeClass('active');
+
+      document.title = 'Midterm Exam';
+    });
 
   const target = $('#pop-target');
   const pop = $('#pop');
@@ -23,16 +28,17 @@ $(() => {
     ({ currentTarget: $this }) => $('#thumbnail').css('background-image', `url('${$($this).attr('href')}')`),
     () => $('#thumbnail').css('background-image', '')
   );
-}).on('keyup', e => {
-  const code = e.keyCode || e.which || e.charCode;
-  const toggle = id => {
-    const el = $(`.collapse[id=${id}]`);
+})
+  .on('keyup', e => {
+    const code = e.keyCode || e.which || e.charCode;
+    const toggle = id => {
+      const el = $(`.collapse[id=${id}]`);
 
-    if (el.hasClass('show'))
-      return el.collapse('hide');
+      if (el.hasClass('show'))
+        return el.collapse('hide');
 
-    el.collapse('show');
-  };
+      el.collapse('show');
+    };
 
-  return toggle(`hb${code}`);
-});
+    return toggle(`hb${code}`);
+  });
